daf714353d1015509677c52c181ed30b8658b022
Noah Ojile <noahojile04@gmail.com>
2025-04-29 01:51:57 +0100

    Update experiences.ts

--- client/src/data/experiences.ts
+++ client/src/data/experiences.ts
@@ -10,11 +10,11 @@ export const experiences: Experience[] = [
     title: "Freelance Web Developer",
     company: "Self-employed",
     period: "2022 - Present",
-    description: "Building MVPs, web apps, and automation tools."
+    description: "Building MVPs, web apps, and Ai automation tools."
   },
   {
-    title: "Python developer (internship)",
-    company: "FUT Minna University Lab",
+    title: "Junior Python developer (internship)",
+    company: "Federal University of Technology Minna",
     period: "2022 - 2023",
     description: "Worked on data projects and research tools."
   },
